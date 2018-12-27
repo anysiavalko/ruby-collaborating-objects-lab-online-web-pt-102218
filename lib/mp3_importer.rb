1d662f4d5eccc45d65c284ccd460dfca23013dda
Anysia V <anysiavalko@gmail.com>
2018-12-27 03:06:06 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,25 @@
+class MP3Importer
+  attr_accessor :path
+  
+  def initialize(path)
+    @path = path
+    @files = []
+  end
+  
+  def files
+    Dir.new(path).each do |file|
+      if file.length > 4
+        @files << file
+      end
+    end
+    files
+  end
+  
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+  end
+  
+  
+end
